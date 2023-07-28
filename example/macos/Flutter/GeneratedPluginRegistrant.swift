b2d279bc95c113d2bc50021048d2a8ee8241015f
David Londono <davidlondono9@gmail.com>
2023-07-28 11:50:17 -0500

    pigeon install

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_contacts_plus
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterContactsPlusPlugin.register(with: registry.registrar(forPlugin: "FlutterContactsPlusPlugin"))
+}
---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_contacts_plus/flutter_contacts_plus_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterContactsPlusPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterContactsPlusPluginCApi"));
+}
